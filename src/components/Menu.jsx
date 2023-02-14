2cbbe8d3cac911b76b1f39ccea6fc209e5cc63c5
Trang007 <dangthuytrang007@gmail.com>
2023-02-14 17:07:09 +0700

    test2

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,25 +4,45 @@ import SearchIcon from "@mui/icons-material/Search";
 import EmailIcon from "@mui/icons-material/Email";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 const Menu = () => {
+  const menu = [
+    {
+      link: "/",
+      title: "Sản phẩm",
+      notIcon: true,
+    },
+    {
+      link: "/",
+      title: "Giải pháp",
+      notIcon: true,
+    },
+    {
+      link: "/",
+      title: "Thư viện",
+      notIcon: true,
+    },
+    {
+      link: "/",
+      title: "Hỗ trợ",
+      notIcon: true,
+    },
+    {
+      link: "/",
+      title: "Phân phối và bảo hành",
+      notIcon: false,
+    },
+  ];
   return (
     <div className="Menu">
       <div className="Logo">
         <img src={Logo} />
       </div>
       <div className="menu">
-        <a href="">
-          Sản phẩm <ExpandMoreIcon />
-        </a>
-        <a href="">
-          Giải pháp <ExpandMoreIcon />
-        </a>
-        <a href="">
-          Thư viện <ExpandMoreIcon />
-        </a>
-        <a href="">
-          Hỗ trợ <ExpandMoreIcon />
-        </a>
-        <a href="">Phân phối và bảo hành</a>
+        {menu.map((item, index) => (
+          <a href={item.link}>
+            {item.title}
+            {item.notIcon && <ExpandMoreIcon />}
+          </a>
+        ))}
       </div>
       <div className="collapse">
         <button>
