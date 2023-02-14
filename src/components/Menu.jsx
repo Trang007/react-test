4af2ff1b160193796bc1f5d80d29c91cfbf4dc62
Trang007 <dangthuytrang007@gmail.com>
2023-02-14 17:42:53 +0700

    test3

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,44 +4,45 @@ import SearchIcon from "@mui/icons-material/Search";
 import EmailIcon from "@mui/icons-material/Email";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 const Menu = () => {
-  const menu = [
-    {
-      link: "/",
-      title: "Sản phẩm",
-      notIcon: true,
-    },
-    {
-      link: "/",
-      title: "Giải pháp",
-      notIcon: true,
-    },
-    {
-      link: "/",
-      title: "Thư viện",
-      notIcon: true,
-    },
-    {
-      link: "/",
-      title: "Hỗ trợ",
-      notIcon: true,
-    },
-    {
-      link: "/",
-      title: "Phân phối và bảo hành",
-      notIcon: false,
-    },
-  ];
+    const menu =[
+        {
+            link:"/",
+            title:"Sản phẩm",
+            notIcon: true,
+        },
+        {
+            link:"/",
+            title:"Giải pháp",
+            notIcon: true,
+        },
+        {
+            link:"/",
+            title:"Thư viện",
+            notIcon: true,
+        },
+        {
+            link:"/",
+            title:"Hỗ trợ",
+            notIcon: true,
+        },
+
+        {
+            link:"/",
+            title:"Phân phối và bảo hành",
+            notIcon: false,
+        },
+    ];
   return (
     <div className="Menu">
       <div className="Logo">
         <img src={Logo} />
       </div>
       <div className="menu">
-        {menu.map((item, index) => (
-          <a href={item.link}>
-            {item.title}
-            {item.notIcon && <ExpandMoreIcon />}
-          </a>
+        {menu.map((item,index) => (
+           <a href={item.link}>
+           {item.title}
+           {item.notIcon &&  <ExpandMoreIcon />}
+         </a> 
         ))}
       </div>
       <div className="collapse">
